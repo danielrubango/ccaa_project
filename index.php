b1676ee2a222b04f0a20572ed27f3a059fcafee1
Daniel Rubango <danielrubango@gmail.com>
2016-04-17 01:12:49 +0200

    Initial commit to heroku

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenu - CCAA RDC</title>
+	<link rel="stylesheet" type="text/css" href="./css/main.css">
+	<link href='https://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Actor' rel='stylesheet' type='text/css'>
+	<!-- <link href='https://fonts.googleapis.com/css?family=Sacramento' rel='stylesheet' type='text/css'> -->
+</head>
+<body>
+	<div class="page">
+		<header class="page__header">
+			<nav class="nav-bar">
+				
+				<div class="nav-bar__brand">
+					<span>CCAA RDC</span>
+				</div>
+
+				<div class="menu">
+					<ul class="menu__container">
+						<li class="nav__item active"><a href="#">HOME</a></li>
+						<li class="nav__item"><a href="#">ABOUT US</a></li>
+						<li class="nav__item"><a href="#">OUR SERVICES</a></li>
+						<li class="nav__item"><a href="#">CONTACT US</a></li>
+					</ul>
+				</div>
+
+			</nav>
+		</header>
+
+		<!-- <section class="carrousel">
+			<div>
+				<div calss="item1">Item 1</div>
+				<div calss="item2">Item 2</div>
+				<div calss="item3">Item 3</div>
+			</div>
+			<div>
+
+			</div>
+		</section> -->
+
+		<section class="page__content">
+			<div class="container">
+
+				<article class="article">
+					<h2 class="page__title">A home page</h2>
+
+					<div class="post">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est rem pariatur impedit ipsum tempore. Consequatur reiciendis impedit laudantium, maxime, amet accusantium quisquam, quo deleniti molestias velit a numquam ipsam.</p>
+					</div>
+					<div class="post">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est rem pariatur impedit ipsum tempore. Consequatur reiciendis impedit laudantium, maxime, amet accusantium quisquam, quo deleniti molestias velit a numquam ipsam.</p>
+					</div>
+					<div class="post">
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est rem pariatur impedit ipsum tempore. Consequatur reiciendis impedit laudantium, maxime, amet accusantium quisquam, quo deleniti molestias velit a numquam ipsam.</p>
+					</div>
+				</article>
+
+				<aside class="home__sidebar--content">
+					<div class="sidebar">
+						<h3>Sidebar here</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, repudiandae repellat, possimus est quis ipsa dolorem accusamus obcaecati sed sit laborum provident suscipit eligendi, officiis fugit saepe ducimus, asperiores ea.
+						</p>
+					</div>
+
+					<div class="sidebar">
+						<h3>Sidebar here</h3>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, repudiandae repellat, possimus est quis ipsa dolorem accusamus obcaecati sed sit laborum provident suscipit eligendi, officiis fugit saepe ducimus, asperiores ea.
+						</p>
+					</div>
+				</aside>
+
+			</div>
+
+		</section>
+
+		<footer class="page__footer">
+			<div class="page__footer--link">
+				<div class="container">
+
+					Somethign here...
+
+				</div>
+			</div>
+			<div class="page__footer--copyright">
+				<div class="container">Tout droits reserves</div>
+			</div>
+		</footer>
+
+	</div>
+</body>
+</html>
