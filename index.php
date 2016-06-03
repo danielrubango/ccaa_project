3c2d799f6bd8d8b7cd7e4603eb841e4ed39e04d2
Daniel Rubango <danielrubango@gmail.com>
2016-06-03 12:35:44 +0200

    Little change on home demo page

--- index.php
+++ index.php
@@ -1,47 +1,29 @@
-<?php $page="home"; ?>
+<!-- Imports tete de page -->
+<?php
+	$page_legend		= "home";
+	$page_title 		= "Accueil";
+	$page_description	= "Services";
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Bienvenu - CCAA RDC</title>
-	<link rel="stylesheet" type="text/css" href="./css/main.css">
-	<link href='https://fonts.googleapis.com/css?family=Playball' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Actor' rel='stylesheet' type='text/css'>
-	<!-- <link href='https://fonts.googleapis.com/css?family=Sacramento' rel='stylesheet' type='text/css'> -->
-</head>
-<body>
-	<div class="page">
-		
-		<!-- Insertion du header du site, avec menu y compris -->
-		<?php require("./includes/header.php"); ?>
+	$page_h2_title		= "Bienvenue";
 
-		<section class="page__content">
-			<div class="container">
+	require("./includes/layouts/top.php");
+?>
 
-				<article class="article">
-					<h2 class="page__title">A home page</h2>
 
-					<div class="post">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est rem pariatur impedit ipsum tempore. Consequatur reiciendis impedit laudantium, maxime, amet accusantium quisquam, quo deleniti molestias velit a numquam ipsam.</p>
-					</div>
-					<div class="post">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est rem pariatur impedit ipsum tempore. Consequatur reiciendis impedit laudantium, maxime, amet accusantium quisquam, quo deleniti molestias velit a numquam ipsam.</p>
-					</div>
-					<div class="post">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est rem pariatur impedit ipsum tempore. Consequatur reiciendis impedit laudantium, maxime, amet accusantium quisquam, quo deleniti molestias velit a numquam ipsam.</p>
-					</div>
-				</article>
-				
-				<!-- Insertion de la sidebar qui revient sur toutes les pages en quelques sortes -->
-				<?php require("./includes/sidebar.php"); ?>
-
-			</div>
-		</section>
-
-		<!-- Insertion du footer du site, contenant aussi le copyright -->
-		<?php require("./includes/footer.php"); ?>
-		
+<!-- Contenu de la page -->
+	<div class="post">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est rem pariatur impedit ipsum tempore. Consequatur reiciendis impedit laudantium, maxime, amet accusantium quisquam, quo deleniti molestias velit a numquam ipsam.</p>
 	</div>
-</body>
-</html>
+
+	<div class="post">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est rem pariatur impedit ipsum tempore. Consequatur reiciendis impedit laudantium, maxime, amet accusantium quisquam, quo deleniti molestias velit a numquam ipsam.</p>
+	</div>
+	
+	<div class="post">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, est rem pariatur impedit ipsum tempore. Consequatur reiciendis impedit laudantium, maxime, amet accusantium quisquam, quo deleniti molestias velit a numquam ipsam.</p>
+	</div>
+<!-- Fin contenu de la page -->
+
+
+<!-- Imports pied de page -->
+<?php require("./includes/layouts/foot.php"); ?>
